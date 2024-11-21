21215b8492affbd2cb89f1f55e3d02574e8b9b2f
maxts <maksTS1991@yandex.ru>
2024-11-21 16:22:34 +0300

    Добавлено решение задачи "Я буду устанавливать все игры!"

--- game_shop/task1/models.py
+++ game_shop/task1/models.py
@@ -5,37 +5,21 @@ from django.db import models
 
 class Buyer(models.Model):
     name = models.CharField(max_length=100)
-    balance = models.DecimalField(decimal_places=8, max_digits=8)
+    balance = models.DecimalField(decimal_places=2, max_digits=8)
     age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
 
 
 class Game(models.Model):
     title = models.CharField(max_length=100)
-    cost = models.DecimalField(decimal_places=8, max_digits=8)
-    size = models.DecimalField(decimal_places=8, max_digits=8)
+    cost = models.DecimalField(decimal_places=2, max_digits=8)
+    size = models.DecimalField(decimal_places=0, max_digits=8)
     description = models.TextField()
     age_limited = models.BooleanField(db_default=False)
-    buyer = models.ManyToManyField(to="Buyer", related_name="games")
+    buyer = models.ManyToManyField(Buyer)
 
+    def __str__(self):
+        return self.title
 
-"""
-Game - модель представляющая игру.
-
-Обладает следующими полями:
-
-title - название игры
-
-cost - цена(DecimalField)
-
-size - размер файлов игры(DecimalField)
-
-description - описание(неограниченное кол-во текста)
-
-age_limited - ограничение возраста 18+ (BooleanField, по умолчанию False)
-
-buyer - покупатель обладающий игрой (ManyToManyField). У каждого покупателя может быть игра и у каждой игры может быть несколько обладателей.
-
-DecimalField - поле для дробных чисел.
-
-BooleanField - поле для булевых значений.
-"""
